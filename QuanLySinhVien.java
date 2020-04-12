2c27f5480b5fe1bd4a4d3bb7ef86278fab5d584a
meocutee123 <49399040+meocutee123@users.noreply.github.com>
2020-04-12 09:27:29 +0700

    Finished!

--- QuanLySinhVien.java
+++ QuanLySinhVien.java
@@ -1,15 +1,15 @@
 import java.util.ArrayList;
-import java.util.Collection;
-
 import mypack.NhanVien;
 
 public class QuanLySinhVien implements IQuanLy {
-    ArrayList<String> listNhanVien = new ArrayList<>();
+    ArrayList<NhanVien> listNhanVien = new ArrayList<NhanVien>();
 
+    @Override
     public void them(NhanVien nv) {
-        listNhanVien.addAll((Collection<? extends String>) nv);
+        listNhanVien.add(nv);
     }
+    @Override
     public void inDS() {
-       // listNhanVien.forEach(nv ->System.out.println(nv.getThongTin()));
+       listNhanVien.forEach(nv ->System.out.println(nv.getThongTin()));
     }
 }
